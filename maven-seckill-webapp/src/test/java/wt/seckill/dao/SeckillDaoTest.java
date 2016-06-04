b44b05e1a3a4179c2659a438a0b14f21cb6b8365
hohoTT <609029365@qq.com>
2016-06-04 12:56:41 +0800

    添加查询某个秒杀表中记录的测试，测试spring与mybatis整合无误

---
+++ maven-seckill-webapp/src/test/java/wt/seckill/dao/SeckillDaoTest.java
@@ -0,0 +1,53 @@
+package wt.seckill.dao;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import wt.seckill.entity.Seckill;
+
+/**
+ * 
+ * @author hohoTT
+ * 配置 spring 与 junit 的整合，junit 启动时加载springIOC容器 -- @RunWith
+ * spring-test,junit
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+// 告诉 junit spring 的配置文件
+@ContextConfiguration({"classpath:spring/spring-dao.xml"})
+public class SeckillDaoTest {
+	
+	// 注入 Dao 实现类依赖
+	@Resource
+	private SeckillDao seckillDao;
+	
+	@Test
+	public void testQueryById() throws Exception {
+		long id = 1000;
+		Seckill seckill = seckillDao.queryById(id);
+		System.out.println(seckill.getName());
+		System.out.println(seckill);
+	}
+	
+	@Test
+	public void testQueryAll() throws Exception {
+		List<Seckill> seckills = seckillDao.queryAll(0, 5);
+		
+		for (Seckill seckill : seckills) {
+			System.out.println(seckill);
+		}
+		
+	}
+	
+	
+	@Test
+	public void testReduceNumber() throws Exception {
+		
+	}
+
+}
