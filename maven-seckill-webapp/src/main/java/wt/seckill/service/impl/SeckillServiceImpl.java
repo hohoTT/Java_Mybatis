99ac29efe3883ad1b57d8305b48cecc1910aaab2
hohoTT <609029365@qq.com>
2016-06-05 10:46:18 +0800

    添加spring中基于注解的声明式事务的配置

--- maven-seckill-webapp/src/main/java/wt/seckill/service/impl/SeckillServiceImpl.java
+++ maven-seckill-webapp/src/main/java/wt/seckill/service/impl/SeckillServiceImpl.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.DigestUtils;
 
 import wt.seckill.dao.SeckillDao;
@@ -84,7 +85,16 @@ public class SeckillServiceImpl implements SeckillService {
 		return md5;
 	}
 
+	/**
+     * 使用注解控制事务的优点:
+     * 1. 开发团队达成一致约定, 明确标注事务方法的编程风格.
+     * 2. 保证事务方法的执行时间尽可能短, 不要穿插其他网络操作 RPC/HTTP 请求或者玻璃到事务方法外部.
+     * 3. 不是所有的方法都需要事务. 
+     * 	如只有一条查询修改操作，只读操作不需要事务控制
+     * 	如一些查询的 service.只有一条修改操作的 service.
+     */
 	@Override
+	@Transactional
 	public SeckillExecution executeSeckill(long seckillId, long userPhone,
 			String md5) throws SeckillException, RepeatKillException,
 			SeckillCloseException {
